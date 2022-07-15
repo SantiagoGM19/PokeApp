ed5ea18aeb26e1a917442c721614b934d6e1eb4c
Santiago Gomez Montoya <santiago.gomez@sofka.local>
2022-07-14 19:43:25 -0500

    adding more styles and making responsive design

--- src/pages/Pokemons.jsx
+++ src/pages/Pokemons.jsx
@@ -1,22 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import { getPokemons } from "../state/PokemonsSlice";
 import "./styles/pokemonsStyles.css";
+import "./styles/pokemonsStylesResponsive.css"
 
 function Pokemons() {
-  const { listOfPokemons } = useSelector((state) => state.pokemons)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const { listOfPokemons } = useSelector((state) => state.pokemons);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [page, setPage] = useState(1);
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonFound, setPokemonFound] = useState({});
 
   useEffect(() => {
     pickPokemons();
-  }, []);
+  }, [page]);
 
   const pickPokemons = async () => {
-    let lowerLimit = Math.floor(Math.random() * 150 + 1);
+    // let lowerLimit = Math.floor(Math.random() * 150 + 1);
     let pokemonList = [];
-    for (let id = lowerLimit; id < lowerLimit + 50; id++) {
+    for (let id = page * 50 - 49; id < page * 50 + 1; id++) {
       let pokemon = await fetchPokemons(id);
       pokemonList.push(pokemon);
     }
@@ -24,23 +28,101 @@ function Pokemons() {
     dispatch(getPokemons(pokemonList));
   };
 
-  const fetchPokemons = async (id) => {
-    let data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const pickOnePokemon = async (e) => {
+    e.preventDefault();
+    let pokemon = await fetchPokemons(pokemonName);
+    if (pokemon) {
+      setPokemonFound(pokemon);
+    }
+  };
+
+  const fetchPokemons = async (item) => {
+    let data = await fetch(`https://pokeapi.co/api/v2/pokemon/${item}`);
     let pokemon = data.json();
     return pokemon;
   };
 
+  const goBack = () => {
+    if (page === 1) {
+      return;
+    }
+    setPage(page - 1);
+  };
+
+  const goForward = () => {
+    if (page === 4) {
+      return;
+    }
+    setPage(page + 1);
+  };
+
+  const onChangePokemonName = (e) => {
+    setPokemonName(e.target.value);
+  };
+
+  const showPokemons = () => {
+    return (
+      <div className="pokemon-list">
+        {listOfPokemons.map((pokemon) => {
+          return (
+            <button
+              key={pokemon.name}
+              className="poke-card"
+              onClick={() => navigate(`/pokemons/${pokemon.id}`)}
+            >
+              <div className="poke-img">
+                <img src={pokemon.img} alt="" />
+              </div>
+              <div className="pokemon-name">{pokemon.name}</div>
+              <div className="types">
+                {pokemon.types.map((type) => {
+                  return (
+                    <div key={type.type.name}>
+                      <div className="type-name">{type.type.name}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </button>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
-    <div className="pokemon-list">
-      {listOfPokemons.map((pokemon) => {
-        return (
-          <button key={pokemon.name} className="poke-card" onClick={() => navigate(`/pokemons/${pokemon.id}`)}>
+    <div className="body-pokemons">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Find your favorite pokemon"
+          onChange={onChangePokemonName}
+        />
+        <div className="search-icon">
+          <button className="button-search" onClick={pickOnePokemon}>
+            <img
+              src="../../src/assets/search-interface-symbol.png"
+              alt=""
+              className="search-img"
+            />
+          </button>
+        </div>
+      </div>
+      {pokemonName === "" ? (
+        showPokemons()
+      ) : JSON.stringify(pokemonFound) !== "{}" ? (
+        <div>
+          <button
+            key={pokemonFound?.name}
+            className="poke-card"
+            onClick={() => navigate(`/pokemons/${pokemonFound?.id}`)}
+          >
             <div className="poke-img">
-              <img src={pokemon.img} alt="" />
+              <img src={pokemonFound?.sprites?.front_default} alt="" />
             </div>
-            <div className="pokemon-name">{pokemon.name}</div>
+            <div className="pokemon-name">{pokemonFound?.name}</div>
             <div className="types">
-              {pokemon.types.map((type) => {
+              {pokemonFound?.types?.map((type) => {
                 return (
                   <div key={type.type.name}>
                     <div className="type-name">{type.type.name}</div>
@@ -49,8 +131,19 @@ function Pokemons() {
               })}
             </div>
           </button>
-        );
-      })}
+        </div>
+      ) : (
+        showPokemons()
+      )}
+      <div className="container-change-page">
+        <button className="button-change-page" onClick={goBack}>
+          &lt;
+        </button>
+        {page}
+        <button className="button-change-page" onClick={goForward}>
+          &gt;
+        </button>
+      </div>
     </div>
   );
 }
